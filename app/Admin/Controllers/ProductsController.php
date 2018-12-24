a2a1c555fb827efe2e25e9829c02a24e552ba574
Hans941 <ahans941@gmail.com>
2018-12-24 18:04:21 +0800

    新增编辑商品

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -26,23 +26,20 @@ class ProductsController extends Controller
     {
         return $content
             ->header('商品详细')
-            ->description('description')
             ->body($this->detail($id));
     }
 
     public function edit($id, Content $content)
     {
         return $content
-            ->header('Edit')
-            ->description('description')
+            ->header('商品编辑')
             ->body($this->form()->edit($id));
     }
 
     public function create(Content $content)
     {
         return $content
-            ->header('Create')
-            ->description('description')
+            ->header('创建商品')
             ->body($this->form());
     }
 
@@ -74,37 +71,32 @@ class ProductsController extends Controller
         return $grid;
     }
 
-    protected function detail($id)
-    {
-        $show = new Show(Product::findOrFail($id));
-
-        $show->id('Id');
-        $show->title('Title');
-        $show->description('Description');
-        $show->image('Image');
-        $show->on_sale('On sale');
-        $show->rating('Rating');
-        $show->sold_count('Sold count');
-        $show->review_count('Review count');
-        $show->price('Price');
-        $show->created_at('Created at');
-        $show->updated_at('Updated at');
-
-        return $show;
-    }
-
     protected function form()
     {
         $form = new Form(new Product);
 
-        $form->text('title', 'Title');
-        $form->textarea('description', 'Description');
-        $form->image('image', 'Image');
-        $form->switch('on_sale', 'On sale')->default(1);
-        $form->decimal('rating', 'Rating')->default(5.00);
-        $form->number('sold_count', 'Sold count');
-        $form->number('review_count', 'Review count');
-        $form->decimal('price', 'Price');
+        $form->text('title', '商品名称')->rules('required');
+        $form->image('image', '图片')->rules('required|image');
+
+        //创建一个富文本编辑器
+        $form->editor('description', '商品名称')->rules('required');
+//         创建一组单选框
+        $form->radio('on_sale', '上架')->options(['1' => '是', '0'=> '否'])->default('0');
+
+        // 直接添加一对多的关联模型
+        $form->hasMany('skus', '商品SKU',function (Form\NestedForm $form) {
+            $form->text('title', 'SKU 名称')->rules('required');
+            $form->text('description', 'SKU 描述')->rules('required');
+            $form->text('price', '单价')->rules('required|numeric|min:0.01');
+            $form->text('stock', '剩余库存')->rules('required|integer|min:0');
+        });
+
+        // 定义事件回调，当模型即将保存时会触发这个回调
+        $form->saving(function (Form $form) {
+            //Form::REMOVE_FLAG_NAME 是因为laravel-admin会将删除的字段依然传回后台,只不过将 Form::REMOVE_FLAG_NAME='_remove_' 设置为了 1
+            //所以 where(Form::REMOVE_FLAG_NAME, 0) 意思就是没被删除的字段
+            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
+        });
 
         return $form;
     }
