f47503597c35842a02b37d6bbaf385bf85f043f9
Hans941 <ahans941@gmail.com>
2018-12-25 17:20:20 +0800

    购物车查看和批量操作

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,9 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\CartItem;
 use App\Http\Requests\AddCartRequest;
+use App\Models\ProductSku;
+use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
+    public function index(Request $request)
+    {
+        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+
+        return view('cart.index', ['cartItems' => $cartItems]);
+    }
+
     public function add(AddCartRequest $request)
     {
         info($request->all());
@@ -32,4 +41,13 @@ class CartController extends Controller
 
         return [];
     }
+
+
+    public function remove(ProductSku $sku, Request $request)
+    {
+        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
+
+        return [];
+    }
+
 }
