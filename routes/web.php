e067619f105e1134bc3d2596bcbbb2d1766a20bd
Hans941 <ahans941@gmail.com>
2018-12-25 13:35:07 +0800

    用户收藏列表

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
 
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Auth::routes();
 
@@ -33,5 +32,9 @@ Route::group(['middleware' => 'auth'], function () {
         Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
-        Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');    });
+        Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+        Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+    });
 });
+
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
