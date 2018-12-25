9826a9ecb3076f8e11cfa312658b3f8261ddf21f
Hans941 <ahans941@gmail.com>
2018-12-25 11:47:44 +0800

    商品详情页

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', 'PagesController@root')->name('root');
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
 
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
