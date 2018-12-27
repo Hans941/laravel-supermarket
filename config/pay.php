5a0a3683e44340069164b692f1c529c37b5b4413
Hans941 <ahans941@gmail.com>
2018-12-27 15:36:56 +0800

    引入支付库(仅使用支付宝)

---
+++ config/pay.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'alipay' => [
+        'app_id'         => '',
+        'ali_public_key' => '',
+        'private_key'    => '',
+        'log'            => [
+            'file' => storage_path('logs/alipay.log'),
+        ],
+    ],
+];
