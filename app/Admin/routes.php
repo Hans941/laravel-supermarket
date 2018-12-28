83c3a124c96f76bba107fe6f23be4ca45721330b
Hans941 <ahans941@gmail.com>
2018-12-28 15:38:54 +0800

    git commit -m "管理后台 - 订单发货"

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -12,6 +12,6 @@ Route::group([
     $router->get('/', 'HomeController@index');
     $router->get('users', 'UsersController@index');
     $router->resource('products', ProductsController::class)->except('show');
-    $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
-    $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
+    $router->resource('orders', OrdersController::class, ['names' => 'admin.orders'])->only('index', 'show');
+    $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
 });
